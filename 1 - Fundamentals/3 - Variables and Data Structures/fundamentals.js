d3cd4376fb9a7615d3eabe713841092664b40c54
Helena Makendengue <helenamakens1013@gmail.com>
2022-10-04 16:20:44 -0500

    code for js fundamentals

---
+++ 1 - Fundamentals/3 - Variables and Data Structures/fundamentals.js
@@ -0,0 +1,108 @@
+/* Uncomment each example one by one to test
+out the code using the following command: 
+node fundamentals.js */
+
+// EXAMPLE 1
+
+// console.log(x);
+// var x;
+
+// EXAMPLE 2
+
+// console.log(x);
+// console.log(y);
+// function example() {
+//   var x;
+// }
+// var y;
+
+// EXAMPLE 3
+
+// console.log(y);
+// function example() {
+//   var x;
+// }
+// var y;
+
+// EXAMPLE 4
+
+// var y;
+// console.log(y);
+// function example() {
+//   var x;
+//   // Other stuff happening here
+//   x = "Hello";
+// }
+// y = 10;
+// console.log(y);
+
+// EXAMPLE 5
+
+// var y;
+// console.log(y);
+// function example() {
+//   var x;
+//   console.log(x);
+//   // Other stuff happening here
+//   x = "Hello";
+//   console.log(x);
+// }
+// y = 10;
+// console.log(y);
+
+// EXAMPLE 6
+
+// var y;
+// console.log("Y: " + y);
+// function example() {
+//   var x;
+//   console.log("X: " + x);
+//   // Other stuff happening here
+//   x = "Hello";
+//   console.log("X: " + x);
+// }
+// y = 10;
+// console.log("Y: " + y);
+
+// EXAMPLE 7
+
+// var y;
+// console.log("Y: " + y);
+// function example() {
+//   var x;
+//   console.log("X: " + x);
+//   // Other stuff happening here
+//   x = "Hello";
+//   console.log("X: " + x);
+// }
+// y = 10;
+// console.log("Y: " + y);
+// example();
+
+// EXAMPLE 8
+
+// for (var i = 0; i < 10; i++) {
+//   console.log("Running in loop: " + i);
+// }
+// console.log("Running outside the loop: " + i);
+// i = 500;
+// console.log("Running outside the loop: " + i);
+
+// EXAMPLE 9
+
+// for (let i = 0; i < 10; i++) {
+//   console.log("Running in loop: " + i);
+// }
+// console.log("Running outside the loop: " + i);
+// i = 500;
+// console.log("Running outside the loop: " + i);
+
+// EXAMPLE 10
+
+// let i;
+// for (i = 0; i < 10; i++) {
+//   console.log("Running in loop: " + i);
+// }
+// console.log("Running outside the loop: " + i);
+// i = 500;
+// console.log("Running outside the loop: " + i);
---
+++ 1 - Fundamentals/5 - Functions/fundamentals.js
@@ -0,0 +1,39 @@
+/* Uncomment each example one by one to test
+out the code using the following command: 
+node fundamentals.js */
+
+// EXAMPLE 1
+
+// function add(x, y) {
+//   return x + y;
+// }
+// console.log(add());
+// console.log(add(1));
+// console.log(add(1, 2));
+// console.log(add("Hello ", "World"));
+// console.log(add(true, false));
+//console.log(add(1, 2, 3, 4, 5, 6, 7, 8, 9, 10));
+
+// EXAMPLE 2
+
+// function add(x, y, z) {
+//   return x + y + z;
+// }
+// console.log(add(1, 2, 3, 4, 5, 6, 7, 8, 9, 10));
+
+// EXAMPLE 3
+
+// function add() {
+//   console.log(arguments);
+//   let sum = 0;
+//   for (let num of arguments) {
+//     sum += num;
+//   }
+//   return sum;
+// }
+// console.log(add());
+// console.log(add(1));
+// console.log(add(1, 2));
+// console.log(add("Hello ", "World"));
+// console.log(add(true, false));
+//console.log(add(1, 2, 3, 4, 5, 6, 7, 8, 9, 10));
